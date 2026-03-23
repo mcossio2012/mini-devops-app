c45deb0c4403f0983bddea14530411334d03fac3
Maximiliano <maximus@Desktop-Maxi.localdomain>
2026-03-23 07:47:59 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import socket
+import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    <h1>Hola Maxi 😎</h1>
+    <p>Hostname: {socket.gethostname()}</p>
+    <p>Fecha: {datetime.datetime.now()}</p>
+    """
+@app.route("/env")
+def env():
+    return f"ENV: {os.getenv('ENVIRONMENT', 'dev')}"
+
+app.run(host="0.0.0.0", port=5000)
